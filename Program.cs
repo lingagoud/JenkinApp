402d26bd2d060e386fa41881a934fc315c92d1ea
lingagoud <lingagoud1279@gmail.com>
2022-07-15 11:48:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace JenkinApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Jenkins");
+            Console.ReadLine();
+        }
+    }
+}
